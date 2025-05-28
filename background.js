56f4aad25d773d5248812d75f264eedf7a09627c
hndrbs <hndrbudisantoso@gmail.com>
2025-05-28 23:06:31 +0700

    add changeUrl handler to current tab

--- background.js
+++ background.js
@@ -1,22 +1,35 @@
-const takeCaredOfDomains = [
+const domainsToKeep = [
     'tv16.nontondrama.click',
     'tv4.lk21official.cc'
 ]; 
 
+function isDomainAllowed(sourceUrl, newUrl) {
+    const sourceDomain = new URL(sourceUrl).hostname;
+    const newDomain = new URL(newUrl).hostname;
+    return domainsToKeep.includes(sourceDomain) && sourceDomain === newDomain;
+}
+
+
 chrome.webNavigation.onCreatedNavigationTarget.addListener((details) => {
   // Get the source tab that initiated the new tab
   chrome.tabs.get(details.sourceTabId, (sourceTab) => {
-    // Get the domain of the source tab
-    const sourceDomain = new URL(sourceTab.url).hostname;
-    // Get the domain of the new tab
-    const newTabDomain = new URL(details.url).hostname;
-
-    console.log(`Source Domain: ${sourceDomain}`);
-    console.log(`New Tab Domain: ${newTabDomain}`);
-
-    // If domains don't match, close the new tab
-    if (takeCaredOfDomains.includes(sourceDomain) && sourceDomain !== newTabDomain) {
+    if (!isDomainAllowed(sourceTab.url, details.url)) {
       chrome.tabs.remove(details.tabId);
     }
   });
+});
+
+chrome.tabs.onUpdated.addListener((tabId, changeInfo, _) => {
+  if (changeInfo.url) {
+    // Get the original URL from the tab's opener
+    chrome.tabs.get(tabId, (currentTab) => {
+      if (currentTab.openerTabId) {
+        chrome.tabs.get(currentTab.openerTabId, (openerTab) => {
+          if (!isDomainAllowed(openerTab.url, changeInfo.url)) {
+            chrome.tabs.remove(tabId)
+          }
+        });
+      }
+    });
+  }
 });
