ab80fd56f750d09c14e96080ff3905cffa3d0d88
hndrbs <hndrbudisantoso@gmail.com>
2025-05-28 23:24:42 +0700

    fix undefiend currentTab error

--- background.js
+++ background.js
@@ -3,29 +3,30 @@ const domainsToKeep = [
     'tv4.lk21official.cc'
 ]; 
 
-function isDomainAllowed(sourceUrl, newUrl) {
+function isDomainBlocked(sourceUrl, newUrl) {
     const sourceDomain = new URL(sourceUrl).hostname;
     const newDomain = new URL(newUrl).hostname;
-    return domainsToKeep.includes(sourceDomain) && sourceDomain === newDomain;
+    return domainsToKeep.includes(sourceDomain) && sourceDomain !== newDomain;
 }
 
 
 chrome.webNavigation.onCreatedNavigationTarget.addListener((details) => {
   // Get the source tab that initiated the new tab
   chrome.tabs.get(details.sourceTabId, (sourceTab) => {
-    if (!isDomainAllowed(sourceTab.url, details.url)) {
+    if (isDomainBlocked(sourceTab.url, details.url)) {
       chrome.tabs.remove(details.tabId);
     }
   });
 });
 
 chrome.tabs.onUpdated.addListener((tabId, changeInfo, _) => {
+  console.log({ tabId, changeInfo });
   if (changeInfo.url) {
     // Get the original URL from the tab's opener
     chrome.tabs.get(tabId, (currentTab) => {
-      if (currentTab.openerTabId) {
+      if (currentTab && currentTab.openerTabId) {
         chrome.tabs.get(currentTab.openerTabId, (openerTab) => {
-          if (!isDomainAllowed(openerTab.url, changeInfo.url)) {
+          if (isDomainBlocked(openerTab.url, changeInfo.url)) {
             chrome.tabs.remove(tabId)
           }
         });
